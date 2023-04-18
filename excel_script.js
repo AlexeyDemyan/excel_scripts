2bbf8fa4073a6c438036778bdfc61067600ce673
Alexey Demyanov <demonoid@bk.ru>
2023-04-18 12:43:11 +0200

    Add files via upload

---
+++ excel_script.js
@@ -0,0 +1,67 @@
+function replaceString(inputString: string) {
+  let newString = '';
+
+  let replaced = false;
+
+  for (let i = 0; i < inputString.length; i++) {
+    if (inputString[i] === '3' && replaced === false) {
+      newString += '';
+      replaced = true;
+    } else {
+      newString += inputString[i];
+    }
+  }
+  return newString;
+}
+
+function main(workbook: ExcelScript.Workbook) {
+  let selectedSheet = workbook.getActiveWorksheet();
+
+  let range = selectedSheet.getRange('B5754:B9369');
+
+  for (let i = 0; i < range.getCellCount(); i++) {
+    let result = range.getCell(i, 0).getText();
+
+    let currentCell = range.getCell(i, 0);
+    currentCell.setValue(replaceString(result));
+  }
+}
+
+function checkValue(inputString: string) {
+  let checked = false;
+
+  for (let i = 0; i < inputString.length; i++) {
+    if (checked === false) {
+      if (
+        inputString[i] === '0' ||
+        inputString[i] === '1' ||
+        inputString[i] === '2' ||
+        inputString[i] === '3' ||
+        inputString[i] === '4' ||
+        inputString[i] === '5' ||
+        inputString[i] === '6' ||
+        inputString[i] === '7' ||
+        inputString[i] === '8' ||
+        inputString[i] === '9'
+      ) {
+        checked = true;
+        if (inputString[i] !== '3') {
+          console.log(inputString);
+        }
+      }
+    }
+  }
+}
+
+function main(workbook: ExcelScript.Workbook) {
+  let selectedSheet = workbook.getActiveWorksheet();
+
+  let range = selectedSheet.getRange('B5754:B9369');
+
+  for (let i = 0; i < range.getCellCount(); i++) {
+    let result = range.getCell(i, 0).getText();
+    checkValue(result);
+  }
+
+  console.log('Done !');
+}
