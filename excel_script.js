a0322e19b949b636708b62ea8356564ed79179c3
Alexey Demyanov <deal110988@him-mail.ch>
2023-04-25 16:00:30 +0200

    Update excel_script.js
    Added script to find and remove certain row

--- excel_script.js
+++ excel_script.js
@@ -112,3 +112,36 @@ function main(workbook: ExcelScript.Workbook) {
 
     console.log('Done !')
 }
+
+// Find certain string and remove the entire row
+
+// Input to adjust
+const CHOSEN_RANGE = "A3:A880";
+const STRING_TO_CHECK = "Total"
+
+// Code to run
+
+function isStringInCellText(inputString: string) {
+  return inputString.includes(STRING_TO_CHECK);
+}
+
+function main(workbook: ExcelScript.Workbook) {
+  let selectedSheet = workbook.getActiveWorksheet();
+
+  let range = selectedSheet.getRange(CHOSEN_RANGE);
+
+  for (let i = 0; i < range.getCellCount(); i++) {
+    let currentCell = range.getCell(i, 0);
+    let result = currentCell.getText();
+    if (isStringInCellText(result)) {
+      console.log(result);
+      let rowToDelete = currentCell.getRowIndex() + 1;
+      let cellsToDelete = 'A' + rowToDelete + ':T' + rowToDelete;
+      console.log(cellsToDelete)
+      let rangeToDelete = selectedSheet.getRange(cellsToDelete);
+      rangeToDelete.delete(ExcelScript.DeleteShiftDirection.up);
+    }
+  };
+
+  console.log('Done !')
+}
