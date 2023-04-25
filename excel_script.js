a442f5738cc2f8d7edc13a521ff0830ae81b9eda
Alexey Demyanov <deal110988@him-mail.ch>
2023-04-25 15:25:01 +0200

    Update excel_script.js
    Added 3rd script, update consts in previous scripts, added titles and separated with comments

--- excel_script.js
+++ excel_script.js
@@ -1,67 +1,114 @@
+// Change character in a cell
+
+// amend input parameters here:
+const requieredRange = "B1:B27";
+const characterToRemove = "3"
+// ----------------------------
+
+
 function replaceString(inputString: string) {
-  let newString = '';
 
-  let replaced = false;
+    let newString = "";
 
-  for (let i = 0; i < inputString.length; i++) {
-    if (inputString[i] === '3' && replaced === false) {
-      newString += '';
-      replaced = true;
-    } else {
-      newString += inputString[i];
+    let replaced = false;
+
+    for (let i = 0; i < inputString.length; i++) {
+      if (inputString[i] === characterToRemove && replaced === false) {
+            newString += "";
+            replaced = true;
+        }
+        else {
+            newString += inputString[i]
+        }
     }
-  }
-  return newString;
+    return newString;
 }
 
 function main(workbook: ExcelScript.Workbook) {
-  let selectedSheet = workbook.getActiveWorksheet();
+    let selectedSheet = workbook.getActiveWorksheet();
 
-  let range = selectedSheet.getRange('B5754:B9369');
+  let range = selectedSheet.getRange(requieredRange);
 
-  for (let i = 0; i < range.getCellCount(); i++) {
-    let result = range.getCell(i, 0).getText();
+  let start = Date.now();
 
-    let currentCell = range.getCell(i, 0);
-    currentCell.setValue(replaceString(result));
-  }
+    for (let i = 0; i < range.getCellCount(); i++) {
+        let result = range.getCell(i, 0).getText();
+        
+        let currentCell = range.getCell(i, 0);
+        currentCell.setValue(replaceString(result));
+    };
+
+  let end = Date.now();
+
+  console.log(`Executed in: ${(end - start) * 0.001} seconds`);
 }
+
+// Check If Certain Number Comes Immediately After Alphabet Characters
 
 function checkValue(inputString: string) {
-  let checked = false;
 
-  for (let i = 0; i < inputString.length; i++) {
-    if (checked === false) {
-      if (
-        inputString[i] === '0' ||
-        inputString[i] === '1' ||
-        inputString[i] === '2' ||
-        inputString[i] === '3' ||
-        inputString[i] === '4' ||
-        inputString[i] === '5' ||
-        inputString[i] === '6' ||
-        inputString[i] === '7' ||
-        inputString[i] === '8' ||
-        inputString[i] === '9'
-      ) {
-        checked = true;
-        if (inputString[i] !== '3') {
-          console.log(inputString);
+    let checked = false;
+
+    for (let i = 0; i < inputString.length; i++) {
+        if (checked === false) {
+            if (
+                inputString[i] === "0" ||
+                inputString[i] === "1" ||
+                inputString[i] === "2" ||
+                inputString[i] === "3" ||
+                inputString[i] === "4" ||
+                inputString[i] === "5" ||
+                inputString[i] === "6" ||
+                inputString[i] === "7" ||
+                inputString[i] === "8" ||
+                inputString[i] === "9" 
+            ) {
+                checked = true;
+                if (inputString[i] !== "3") {
+                    console.log(inputString);
+                }
+            }
         }
-      }
     }
-  }
 }
 
 function main(workbook: ExcelScript.Workbook) {
-  let selectedSheet = workbook.getActiveWorksheet();
+    let selectedSheet = workbook.getActiveWorksheet();
 
-  let range = selectedSheet.getRange('B5754:B9369');
+    let range = selectedSheet.getRange("B5754:B9369");
 
-  for (let i = 0; i < range.getCellCount(); i++) {
-    let result = range.getCell(i, 0).getText();
-    checkValue(result);
-  }
+    for (let i = 0; i < range.getCellCount(); i++) {
+        let result = range.getCell(i, 0).getText();
+        checkValue(result);
+    };
 
-  console.log('Done !');
+    console.log('Done !')
+}
+
+// Look for certain string
+
+// Input to adjust
+const CHOSEN_RANGE = "A3:A880";
+const STRING_TO_CHECK = "Total"
+
+// Code to run
+
+function checkValue(inputString: string) {
+
+    if (inputString.includes(STRING_TO_CHECK)) {
+        console.log(inputString)
+    }
+}
+
+function main(workbook: ExcelScript.Workbook) {
+    let selectedSheet = workbook.getActiveWorksheet();
+
+    let range = selectedSheet.getRange(CHOSEN_RANGE);
+
+    for (let i = 0; i < range.getCellCount(); i++) {
+        let result = range.getCell(i, 0).getText();
+        checkValue(result);
+    };
+
+    console.log('Done !')
 }
