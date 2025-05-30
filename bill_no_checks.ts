b779d505a8f1ab488012e8868b7f76d82b9ff201
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-30 10:45:19 +0200

    Update bill_no_checks.ts
    Adjusted for PET

--- bill_no_checks.ts
+++ bill_no_checks.ts
@@ -3,15 +3,20 @@ function main(workbook: ExcelScript.Workbook) {
     // Input to adjust
 
     let sourceSheet = workbook.getActiveWorksheet();
-
-    const RESULT = 'result';
+    const MATERIAL_RECEIPT = 'Material Receipt';
+    const MATERIAL_ISSUE = 'Material Issue';
 
     const RANGE = {
         columnStart: 'A',
-        columnFinish: 'C',
+        columnFinish: 'E',
         rowStart: 1,
-        rowFinish: 4000,
+        rowFinish: 10000,
     };
+
+    const HEADERS = ["Stock Entry Type", "Company", "Item Code (Items)", "Qty (Items)", "Qty as per Stock UOM (Items)", "UOM (Items)", "Stock UOM (Items)", "Conversion Factor (Items)", "Source Warehouse (Items)"];
+
+    const COMPANY = 'Marsovin Winery Ltd (B&V)';
+    const DEFAULT_WAREHOUSE = 'RM (PET) - M'
 
     // Code to run 
 
@@ -19,63 +24,79 @@ function main(workbook: ExcelScript.Workbook) {
 
     let worksheets = workbook.getWorksheets();
     worksheets.forEach((sheet) => {
-        if (sheet.getName() === RESULT) {
+        if (sheet.getName() === MATERIAL_RECEIPT || sheet.getName() === MATERIAL_ISSUE) {
             sheet.delete();
         }
     });
 
     // Creating new sheets
 
-    let resultSheet = workbook.addWorksheet();
-    resultSheet.setName(RESULT);
-    let resultSheetTotalRange = resultSheet.getRange('A1:A1');
+    let materialReceiptSheet = workbook.addWorksheet();
+    materialReceiptSheet.setName(MATERIAL_RECEIPT);
+    let materialReceiptSheetTotalRange = materialReceiptSheet.getRange('A1:A1');
+
+    let materialIssueSheet = workbook.addWorksheet();
+    materialIssueSheet.setName(MATERIAL_ISSUE);
+    let materialIssueSheetTotalRange = materialIssueSheet.getRange('A1:A1');
 
     let currentTotalRange = sourceSheet.getRange(`${RANGE.columnStart}${RANGE.rowStart}:${RANGE.columnFinish}${RANGE.rowStart}`);
 
+    // Arranging Headers
+
+    for (let i = 0; i < HEADERS.length; i++) {
+        materialReceiptSheetTotalRange.getCell(0, i).setValue(HEADERS[i]);
+        materialIssueSheetTotalRange.getCell(0, i).setValue(HEADERS[i]);
+        if (i === 8) {
+            materialReceiptSheetTotalRange.getCell(0, i).setValue("Target Warehouse (Items)");
+        }
+    };
+
+    materialReceiptSheetTotalRange.getCell(1, 0).setValue(MATERIAL_RECEIPT);
+    materialReceiptSheetTotalRange.getCell(1, 1).setValue(COMPANY);
+
+    materialIssueSheetTotalRange.getCell(1, 0).setValue(MATERIAL_ISSUE);
+    materialIssueSheetTotalRange.getCell(1, 1).setValue(COMPANY);
+
     // Main Code
 
-    let billNoToCheckAgainst = '69';
-    let possibleDuplicateInvoiceNo = '';
-    let resultLineCount = 1;
-    let partyList: String[] = [];
+    let materialReceiptLineCounter = 1;
+    let materialIssueLineCounter = 1;
 
     for (let i = 0; i < RANGE.rowFinish; i++) {
-        console.log(`checking ${i}`);
         if (currentTotalRange.getCell(i, 0).getText() === "") {
-            console.log(`Script completed at ${i} iterations`);
+            console.log("Finished!")
             break;
         }
+        let currentItem = currentTotalRange.getCell(i, 0).getText();
+        let currentAmount = currentTotalRange.getCell(i, 4).getText();
 
-        let currentBillNo = currentTotalRange.getCell(i, 0).getText();
-        let currentInvoiceNo = currentTotalRange.getCell(i, 1).getText();
-        let currentParty = currentTotalRange.getCell(i, 2).getText();
+        console.log(`Checking item ${currentItem}`)
+        let itemUOM = currentTotalRange.getCell(i, 2).getText();
+        let itemAmount = currentTotalRange.getCell(i, 3).getText();
 
-        if (currentBillNo !== billNoToCheckAgainst) {
-            billNoToCheckAgainst = currentBillNo;
-            partyList = [];
-            partyList.push(currentParty);
-        } else {
-            if (partyList.includes(currentParty)) {
-                if (resultSheetTotalRange.getCell(resultLineCount - 1, 0).getValue() !== currentBillNo) {
-                    resultLineCount++;
-                }
+        let absoluteDifference = Math.abs(Number(currentAmount) - Number(itemAmount));
 
-                if (resultSheetTotalRange.getCell(resultLineCount - 1, 1).getValue() !== possibleDuplicateInvoiceNo) {   
-                resultSheetTotalRange.getCell(resultLineCount, 0).setValue(currentBillNo);
-                resultSheetTotalRange.getCell(resultLineCount, 1).setValue(possibleDuplicateInvoiceNo);
-                resultSheetTotalRange.getCell(resultLineCount, 2).setValue(currentParty);
-                resultLineCount++;
-                }
-                resultSheetTotalRange.getCell(resultLineCount, 0).setValue(currentBillNo);
-                resultSheetTotalRange.getCell(resultLineCount, 1).setValue(currentInvoiceNo);
-                resultSheetTotalRange.getCell(resultLineCount, 2).setValue(currentParty);
-                resultLineCount++;
-            } else {
-                partyList.push(currentParty);
-            }
+        if (Number(currentAmount) > Number(itemAmount)) {
+            materialReceiptSheetTotalRange.getCell(materialReceiptLineCounter, 2).setValue(currentItem);
+            materialReceiptSheetTotalRange.getCell(materialReceiptLineCounter, 3).setValue(absoluteDifference);
+            materialReceiptSheetTotalRange.getCell(materialReceiptLineCounter, 4).setValue(absoluteDifference);
+            materialReceiptSheetTotalRange.getCell(materialReceiptLineCounter, 5).setValue(itemUOM);
+            materialReceiptSheetTotalRange.getCell(materialReceiptLineCounter, 6).setValue(itemUOM);
+            materialReceiptSheetTotalRange.getCell(materialReceiptLineCounter, 7).setValue(1);
+            materialReceiptSheetTotalRange.getCell(materialReceiptLineCounter, 8).setValue(DEFAULT_WAREHOUSE);
+            materialReceiptLineCounter++
         }
-        possibleDuplicateInvoiceNo = currentInvoiceNo;
 
+        if (Number(currentAmount) < Number(itemAmount)) {
+            materialIssueSheetTotalRange.getCell(materialIssueLineCounter, 2).setValue(currentItem);
+            materialIssueSheetTotalRange.getCell(materialIssueLineCounter, 3).setValue(absoluteDifference);
+            materialIssueSheetTotalRange.getCell(materialIssueLineCounter, 4).setValue(absoluteDifference);
+            materialIssueSheetTotalRange.getCell(materialIssueLineCounter, 5).setValue(itemUOM);
+            materialIssueSheetTotalRange.getCell(materialIssueLineCounter, 6).setValue(itemUOM);
+            materialIssueSheetTotalRange.getCell(materialIssueLineCounter, 7).setValue(1);
+            materialIssueSheetTotalRange.getCell(materialIssueLineCounter, 8).setValue(DEFAULT_WAREHOUSE);
+            materialIssueLineCounter++
+        }
     }
 
     console.log('Done !')
